d31905b5569cc23f43343d63d74fb1e9d3ae8fb6
NephygTdkingz <tdkingzict@gmail.com>
2024-04-27 15:13:37 +0100

    added guincorn

--- avahtrans/urls.py
+++ avahtrans/urls.py
@@ -2,10 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls', namespace='frontend')),
     path('shipment/', include('shipment.urls', namespace='shipment')),
 ]
-urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
